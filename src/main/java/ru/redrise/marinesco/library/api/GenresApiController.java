762bb8b97fdb6b3c9511a1c08150e62519651331
Dmitry Isaenko <developer.su@gmail.com>
2024-01-27 22:49:17 +0300

    Add create function

--- src/main/java/ru/redrise/marinesco/library/api/GenresApiController.java
+++ src/main/java/ru/redrise/marinesco/library/api/GenresApiController.java
@@ -2,9 +2,13 @@ package ru.redrise.marinesco.library.api;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ru.redrise.marinesco.data.GenreRepository;
@@ -29,5 +33,10 @@ public class GenresApiController {
         return genreRepository.findAll(pageRequest).getContent();
     }
 
-    
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Genre postGenre(@RequestBody Genre genre) {
+        return genreRepository.save(genre);
+    }
+
 }
